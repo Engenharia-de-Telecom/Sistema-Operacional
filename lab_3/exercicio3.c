4215fae2a60b56c90f0fb98564495b4c431134bf
Tiago <tiago.m2005@aluno.ifsc.edu.br>
2026-03-17 15:32:17 -0300

    Tentando implementar a função execve

--- lab_3/exercicio3.c
+++ lab_3/exercicio3.c
@@ -11,7 +11,51 @@
 #include <stdbool.h>
 #include <string.h>
 
-int main(int argc, char** argv){
-    char caminho[256];
+void encontra_ecomercial(char* entrada[], bool ecomercial){
+// Está função deve alterar o conteûdo da variável ecomercial para true, caso encontre "&" na entrada
 
+
+    
+}
+int main(int argc, char** argv){
+    char entrada[256];
+    int pid, status;
+    while(true){
+        printf("> ");
+        fgets(entrada, 256, stdin);
+    
+        entrada[strlen(entrada) - 1] = '\0'; // substitui o quebra de linha pelo caracter nulo
+
+        bool ecomercial = false;
+        if(entrada[strlen(entrada) - 1] == '&'){ // verifica se '&' esta na entrada
+            ecomercial = true;
+            entrada[strlen(entrada) - 1] = '\0';
+            if((strlen(entrada) > 1) && (entrada[strlen(entrada) - 2] == ' ')){
+                entrada[strlen(entrada) - 2] = '\0';
+            }
+        }
+
+
+        if(strcmp(entrada, "sair") == 0){
+            exit(0);
+        }
+        
+        pid = fork();
+        if(pid == -1){ // erro
+            perror("Fork falhou!");
+
+        } else if(pid == 0){ // filho
+            char *args[] = {entrada, NULL};
+            execve(entrada, args, NULL);
+            perror("Erro");
+            exit(1);
+
+        } else{
+            printf("%d\n", ecomercial);
+            if(ecomercial){
+                printf("Entrando no wait\n");
+                wait(&status);
+        }
+        }
+    }
 }
